56e3d21cbd23b87ddfb64f1673f0b7481f18a5cc
Vatsal Parekh <vparekh@redhat.com>
2021-05-02 13:12:58 +0530

    Initial commit
    Signed-off-by: Vatsal Parekh <vparekh@redhat.com>

---
+++ main.go
@@ -0,0 +1,14 @@
+package cowin_query
+
+var (
+	pincode string
+	inputDate string
+)
+
+func main() {
+	// Parse the input pincode and inputDate from CLI args
+	// Sanitize the inputs
+	// Query API-Setu with the input filters
+	// Find our 18+ slots from the Query data
+	// Output the results
+}
